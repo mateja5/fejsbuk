db466be1edd2a2cb3fe9f24b3e2b604cf3dc24f7
mateja5 <Mateja.Makse@students.fmf.uni-lj.si>
2014-11-17 11:31:44 +0100

    Poskus predloge

---
+++ fejstbuk.py
@@ -0,0 +1,66 @@
+#### poskrbi da se vse zažene (drugace se sploh spletna stran ne nalozi)
+
+import bottle 
+import sqlite3
+
+##primer!!
+
+#@route('/')
+#def vsi_smo_fajni():
+#    return "VsI STE TKO FAJNI!!!"
+
+#@route('/kvadriraj/<x>')
+#def kvadriraj(x):
+#    return str(int(x) ** 2)
+
+bottle.debug(True)
+datoteka_baze = "knjiga_obrazov.sqlite"
+
+###################################################################
+
+## ne naredi format na select stavku!!!!!!
+
+#prejšnja verzija!!
+##@bottle.route('/prijatelji/<id>')
+##@bottle.view('prijatelji')
+##def index(id):
+##    c = baza.cursor()
+##    c.execute(
+##    """SELECT osebe.ime, osebe.priimek FROM
+##       osebe JOIN prijateljstva ON
+##       osebe.id = prijateljstva.drugi
+##       WHERE prijateljstva.prvi = 2
+##    """)
+##    seznam_imen = ", ".join(i + " " + p for (i, p) in c.fetchall())
+##    c.close()
+##    return seznam_imen
+
+@bottle.route('/prijatelji/<id>')
+@bottle.view('prijatelji')
+def prijatelji(id):
+    c = baza.cursor()
+    c.execute("""SELECT ime, priimek FROM osebe WHERE id =?""", [id])
+    oseba=c.fetchone()
+    if oseba is None:
+        c.close()
+        return {'obstaja':False}
+    else:
+        (ime,priimek)=oseba
+        c.execute(
+        """SELECT osebe.ime, osebe.priimek
+           FROM osebe JOIN prijateljstva ON osebe.id = prijateljstva.drugi
+           WHERE prijateljstva.prvi = ?""",[id])
+        prijatelji=c.fetchall()
+        c.close()
+        return {'obstaja':True,
+            'ime':ime,
+            'priimek': priimek,
+            'prijatelji':prijatelji
+            }
+
+###################################################################
+
+#priklopimo se na bazo
+baza = sqlite3.connect(datoteka_baze, isolation_level=None)
+bottle.run(host='localhost', port=8080)
+
---
+++ prijatelji.tpl
@@ -0,0 +1,14 @@
+%if not obstaja:
+	<hl>Oseba ne obstaja</hl>
+%else:
+	%if prijatelji:
+		<hl>Prijatelji osebe {{ ime }} {{ priimek }} so: </hl>
+		<ul>
+			%for (i,p) in prijatelji:
+			<li>{{i}} {{p}}</li>
+			%end
+		</ul>
+	%else:
+		<hl>Oseba {{ ime }} {{ priimek }} nima prijateljev</hl>
+	%end
+%end
